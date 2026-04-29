08263296fd41c62aa26911538e95103a19a56982
Luis-Gonzo-Velez <gadiel2311@outlook.com>
2026-04-29 09:27:59 -0400

    chapter 37

--- streams.js
+++ streams.js
@@ -3,8 +3,26 @@ const server = require("http").createServer();
 
 server.on("request", (req, res) => {
   ///Solution 1
-  fs.readFile("test-file.txt", (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
+  // fs.readFile("./starter/test-file.txt", (err, data) => {
+  //   if (err) console.log(err);
+  //   res.end(data);
+  // });
+
+  //Solution 2: Streams
+  const readable = fs.createReadStream("test-file.txt");
+  readable.on("data", (chunk) => {
+    res.write(chunk);
   });
+  readable.on("end", () => {
+    res.end();
+  });
+  readable.on("error", (err) => {
+    console.log(err);
+    res.statusCode = 500;
+    res.end("File not found");
+  });
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listen...");
 });
